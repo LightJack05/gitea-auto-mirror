d41d1bfd19ecfafd5a1c99b7a7f3fc92048a00f2
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-10-23 23:41:28 +0200

    Load config on startup

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/LightJack05/gitea-auto-mirror/internal/config"
 	"github.com/LightJack05/gitea-auto-mirror/internal/endpoints"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	config.LoadConfigFromEnv()
 	router := gin.Default()
 	router.POST("/repo_create_hook", endpoints.RepoCreatePost)
 	router.Run()
