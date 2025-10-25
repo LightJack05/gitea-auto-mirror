18866afdebc9ba1a17cb183c24c80cd150a1045a
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-10-25 14:15:18 +0200

    Added regex filter for repo names

--- internal/endpoints/repoCreateHook.go
+++ internal/endpoints/repoCreateHook.go
@@ -2,17 +2,36 @@ package endpoints
 
 import (
 	"net/http"
+	"regexp"
 
+	"github.com/LightJack05/gitea-auto-mirror/internal/config"
 	"github.com/LightJack05/gitea-auto-mirror/internal/datastructures"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RepoCreatePost(c *gin.Context) {
-	var createEvent datastructures.Event
+	var createEvent datastructures.RepoCreateEvent
 	if err := c.BindJSON(&createEvent); err != nil {
-		c.Status(http.StatusInternalServerError)
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	if !shouldModifyRepo(createEvent.Repository.FullName) {
+		c.Status(http.StatusNoContent)
+		return
 	}
 
 	c.Status(http.StatusNotImplemented)
 }
+
+func shouldModifyRepo(repoPath string) bool {
+	if config.GetActiveConfig().SourceRepoRegExFilter == "" {
+		return true
+	}
+
+	r := regexp.MustCompile(config.GetActiveConfig().SourceRepoRegExFilter)
+
+	return r.MatchString(repoPath)
+}
+
